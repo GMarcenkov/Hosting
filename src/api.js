99637af5e387035f4a09cc7807038315154aa63c
georgi.marcenkov <georgi.marcenkov@3web-studio.com>
2020-10-13 21:27:33 +0300

    make server

--- src/api.js
+++ src/api.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require('morgan');
-// const path = require('path');
 const serverless = require("serverless-http");
+const socketio = require('socket.io');
+const http = require('http');
 
 require("dotenv").config();
 
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
@@ -56,6 +57,63 @@ const categoryRouter = require("../routes/category");
 app.use("/.netlify/functions/api/v1/category", categoryRouter);
 
 
-module.exports = app;
-module.exports.handler = serverless(app);
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('../routes/chat');
+
+
+const server = http.createServer(app);
+let io = socketio.listen(server, { serveClient: false });
+
+
+app.use(cors());
+
+
+io.on('connect', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name:`${name.name} ${name.familyName}`,nameId:name._id, room });
+
+        if(error) return callback(error);
+
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'чат', text: `${user.name}, вие сте на линия.`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message ,nameId:user.nameId});
+
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
+});
+
+
+server.listen(PORT, console.log(`Server is starting at ${PORT}`));
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
+
+
+
+
+
+
+
+
 
