c1ab82ff6c756b153fcaad455df2f75f52261a9f
georgi.marcenkov <georgi.marcenkov@3web-studio.com>
2021-06-14 04:26:30 +0300

    cors delete

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+
 const mongoose = require("mongoose");
 const morgan = require('morgan');
 const serverless = require("serverless-http");
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
